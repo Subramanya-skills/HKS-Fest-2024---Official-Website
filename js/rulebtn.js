58e063828709e7ef8edb57c9b35ac5226b701a93
Subramanya-skills <144552216+Subramanya-skills@users.noreply.github.com>
2025-11-06 16:24:53 +0530

    Add files via upload

---
+++ js/rulebtn.js
@@ -0,0 +1,8 @@
+// let btn = document.getElementsByClassName('custom-btn')
+
+// let a = [...btn]
+// a.map((b, i) => {
+//     b.addEventListener('click', () => {
+//         b.classList.add('artists-hover')
+//     })
+// })
